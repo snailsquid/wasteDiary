c5e26a6d6ebc56d2c5028eb9090856840dd6d60d
snailsquid <hexagononyt@gmail.com>
2023-02-06 15:54:16 +0700

    fonts and navigation

--- App.tsx
+++ App.tsx
@@ -14,14 +14,32 @@ import * as eva from "@eva-design/eva";
 import { default as theme } from "./src/theme/custom-theme.json";
 import { ApplicationProvider, Layout } from "@ui-kitten/components";
 import { default as mapping } from "./mapping.json";
+import { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import {
+  BottomNavigation,
+  BottomNavigationTab,
+  Icon,
+} from "@ui-kitten/components";
 
 const Tab = createBottomTabNavigator();
+const { Navigator, Screen } = createStackNavigator();
+
+const useBottomNavigationState = (initialState = 0) => {
+  const [selectedIndex, setSelectedIndex] = useState(initialState);
+  return { selectedIndex, onSelect: setSelectedIndex };
+};
+
+const HomeIcon = (props: any) => <Icon {...props} name="home" />;
 
 export default function App() {
   const [loaded] = useFonts({
     Poppins: require("./assets/Poppins.ttf"),
-    Unbounded: require("./assets/Unbounded_Bold.ttf"),
+    Unbounded_Bold: require("./assets/Unbounded_Bold.ttf"),
   });
+
+  const navBar = useBottomNavigationState();
 
   if (!loaded) {
     return null;
@@ -33,7 +51,71 @@ export default function App() {
           theme={{ ...eva.light, ...theme }}
           customMapping={mapping}
         >
-          <Home></Home>
+          <NavigationContainer>
+            <Tab.Navigator
+              initialRouteName="Home"
+              screenOptions={({ route, navigation }: any) => ({
+                headerShown: false,
+                tabBarActiveTintColor: "#1CB447",
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                  position: "absolute",
+                  height: 70,
+                  bottom: 20,
+                  borderRadius: marginAndPadding.mainBorderRadius,
+                  paddingHorizontal: 7,
+                  left: 100,
+                  right: 100,
+                  shadowColor: "#6dbe9b",
+                  shadowOffset: {
+                    width: 0,
+                    height: 3,
+                  },
+                  shadowOpacity: 0.17,
+                  shadowRadius: 3.05,
+                  elevation: 4,
+                },
+                tabBarLabel: navigation.isFocused() ? route.name : "",
+                tabBarIcon: ({ focused, color }: any) => {
+                  let octicons: "star" | "star-fill" | "error" = "error";
+                  let materialIcons:
+                    | "lightbulb"
+                    | "lightbulb-outline"
+                    | "error" = "error";
+                  let ionicons:
+                    | "home"
+                    | "home-outline"
+                    | "star-outline"
+                    | "star"
+                    | "error" = "error";
+                  if (route.name === "Home") {
+                    ionicons = focused ? "home" : "home-outline";
+                  } else if (route.name === "Target") {
+                    ionicons = focused ? "star" : "star-outline";
+                  } else if (route.name === "Tips") {
+                    materialIcons = focused ? "lightbulb" : "lightbulb-outline";
+                  }
+                  if (octicons != "error") {
+                    return <Octicons name={octicons} size={32} color={color} />;
+                  } else if (materialIcons != "error") {
+                    return (
+                      <MaterialCommunityIcons
+                        name={materialIcons}
+                        size={32}
+                        color={color}
+                      />
+                    );
+                  } else if (ionicons != "error") {
+                    return <Ionicons name={ionicons} size={32} color={color} />;
+                  }
+                },
+              })}
+            >
+              <Tab.Screen name="Tips" component={Tips} />
+              <Tab.Screen name="Home" component={Home} />
+              <Tab.Screen name="Target" component={Target} />
+            </Tab.Navigator>
+          </NavigationContainer>
         </ApplicationProvider>
         <StatusBar style="auto" />
       </SafeAreaProvider>
@@ -43,63 +125,7 @@ export default function App() {
 function Tabs() {
   return (
     <SafeAreaView style={styles.containerSafe}>
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={({ route, navigation }) => ({
-          headerShown: false,
-          tabBarActiveTintColor: "#1CB447",
-          tabBarShowLabel: false,
-          tabBarStyle: {
-            position: "absolute",
-            height: 70,
-            bottom: 20,
-            borderRadius: marginAndPadding.mainBorderRadius,
-            paddingHorizontal: 7,
-            left: 100,
-            right: 100,
-            shadowColor: "#6dbe9b",
-            shadowOffset: {
-              width: 0,
-              height: 3,
-            },
-            shadowOpacity: 0.17,
-            shadowRadius: 3.05,
-            elevation: 4,
-          },
-          tabBarLabel: navigation.isFocused() ? route.name : "",
-          tabBarIcon: ({ focused, color }) => {
-            let octicons: "star" | "star-fill" | "error" = "error";
-            let materialIcons: "lightbulb" | "lightbulb-outline" | "error" =
-              "error";
-            let ionicons:
-              | "home"
-              | "home-outline"
-              | "star-outline"
-              | "star"
-              | "error" = "error";
-            if (route.name === "Home") {
-              ionicons = focused ? "home" : "home-outline";
-            } else if (route.name === "Target") {
-              ionicons = focused ? "star" : "star-outline";
-            } else if (route.name === "Tips") {
-              materialIcons = focused ? "lightbulb" : "lightbulb-outline";
-            }
-            if (octicons != "error") {
-              return <Octicons name={octicons} size={32} color={color} />;
-            } else if (materialIcons != "error") {
-              return (
-                <MaterialCommunityIcons
-                  name={materialIcons}
-                  size={32}
-                  color={color}
-                />
-              );
-            } else if (ionicons != "error") {
-              return <Ionicons name={ionicons} size={32} color={color} />;
-            }
-          },
-        })}
-      >
+      <Tab.Navigator>
         <Tab.Screen name="Tips" component={Tips} />
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Target" component={Target} />
@@ -112,5 +138,8 @@ const styles = StyleSheet.create({
   containerSafe: {
     flex: 1,
     backgroundColor: colors.greenSub,
+  },
+  navBar: {
+    marginVertical: 8,
   },
 });
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,20 @@ import { StyleSheet, View, StatusBar as StatBar } from "react-native";
 import colors from "../theme/colors.json";
 import { Button, Text } from "@ui-kitten/components/ui";
 import { Layout } from "@ui-kitten/components";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { TopNavigation } from "@ui-kitten/components";
 
 export default function Home() {
   return (
-    <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Button>Helo</Button>
-      <Text>January</Text>
-      <Text category="h1">Waste Diary</Text>
-    </Layout>
+    <SafeAreaView style={{ flex: 1 }}>
+      <TopNavigation title="Waste Diary" alignment="start" />
+      <Layout
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      >
+        <Button>Helo</Button>
+        <Text>January</Text>
+      </Layout>
+    </SafeAreaView>
   );
 }
 
