c22ca82235621db323f31d918cebb87c3fba3221
snailsquid <hexagononyt@gmail.com>
2023-02-06 14:39:11 +0700

    ui kitten

--- App.tsx
+++ App.tsx
@@ -3,33 +3,41 @@ import { StyleSheet, SafeAreaView } from "react-native";
 import { useFonts } from "expo-font";
 import { Octicons, MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
 import colors from "./src/theme/colors.json";
-import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 import Home from "./src/pages/Home";
 import marginAndPadding from "./src/theme/marginAndPadding.json";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Tips from "./src/pages/Tips";
 import Target from "./src/pages/Target";
+import * as eva from "@eva-design/eva";
+import { default as theme } from "./src/theme/custom-theme.json";
+import { ApplicationProvider, Layout } from "@ui-kitten/components";
+import { default as mapping } from "./mapping.json";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [loaded] = useFonts({
-    Poppins: require("./assets/fonts/Poppins.ttf"),
-    "Unbounded-Bold": require("./assets/fonts/Unbounded-Bold.ttf"),
+    Poppins: require("./assets/Poppins.ttf"),
+    Unbounded: require("./assets/Unbounded_Bold.ttf"),
   });
 
   if (!loaded) {
     return null;
-  }
-  return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Tabs></Tabs>
+  } else
+    return (
+      <SafeAreaProvider>
+        <ApplicationProvider
+          {...eva}
+          theme={{ ...eva.light, ...theme }}
+          customMapping={mapping}
+        >
+          <Home></Home>
+        </ApplicationProvider>
         <StatusBar style="auto" />
-      </NavigationContainer>
-    </SafeAreaProvider>
-  );
+      </SafeAreaProvider>
+    );
 }
 
 function Tabs() {
